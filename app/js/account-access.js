c09fc129cc2d9c5b2354c8df142ab72c7355ee69
Jamie Gilman <jamie@reflection.io>
2015-06-02 14:39:08 +0100

    Improvements to templates complete

--- app/js/account-access.js
+++ app/js/account-access.js
@@ -7,6 +7,10 @@ var templateGlobalHeader = Handlebars.templates['globalHeader'];
 var htmlGlobalHeader = templateGlobalHeader({});
 $("#js-component-import--global-header").html(htmlGlobalHeader);
 
+var templateAppMenu = Handlebars.templates['appMenu'];
+var htmlAppMenu = templateAppMenu({});
+$("#js-component-import--panel-left").html(htmlAppMenu);
+
 var AccountAccess = function() {
 	instance = this;
 	// App Components
